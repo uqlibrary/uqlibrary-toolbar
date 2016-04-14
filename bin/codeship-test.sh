6afa51d469edc3bc28aefb52f0ac8c150ad0dfd2
Cameron Green <cam@uq.edu.au>
2016-04-14 12:15:34 +1000

    Another attempt to deal with tags

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -5,7 +5,7 @@ echo "Testing branch: ${CI_BRANCH}"
 # REDO WHEN POLYMER 1.0 GOES LIVE
 
 # If polymer 1.0
-if [ ${CI_BRANCH} == "polymer1.0" -o ${CI_BRANCH} == "v1*" ]; then
+if [[ ${CI_BRANCH} == "polymer1.0" ]] || [[ ${CI_BRANCH} == "v1*" ]]; then
     if [ ${PIPE_NUM} == 1 ]; then
         # Run local tests
         echo "Starting local WCT tests"
